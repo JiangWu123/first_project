e751211b13aa4e2b2e52589c9ceea4bb20df6e76
wu jiang <516734948@qq.com>
2016-01-04 09:51:19 +0800

    frist commit

---
+++ src/main/java/com/abc/xyz/first_project/App.java
@@ -0,0 +1,13 @@
+package com.abc.xyz.first_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
